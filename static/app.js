44544d6a7760d7f282d84a8701affda470336438
gustrd <gustrd@gmail.com>
2023-03-21 14:38:37 -0300

    feat - basic validation at the form

--- static/app.js
+++ static/app.js
@@ -3,7 +3,7 @@ const Controller = {
     ev.preventDefault();
     const form = document.getElementById("form");
     const data = Object.fromEntries(new FormData(form));
-    const response = fetch(`/search?q=${data.query}`).then((response) => {
+    const response = fetch(`/search?q=${data.query}&s=${data.size}`).then((response) => {
       response.json().then((results) => {
         Controller.updateTable(results);
       });
@@ -32,5 +32,31 @@ $(document).ready(function() {
     pagination: true,
     perPage:5,
     globalSearch:true
+  }),
+
+  $("#form").validate({
+    rules : {
+          query:{
+                 required:true,
+                 minlength:3
+          },
+          size:{
+                 required:true,
+                 min: 50,
+                 max: 600
+          }                              
+    },
+    messages:{
+          query:{
+                 required:"You need to provide the word or sentence to search",
+                 minlength:"The word needs to have at least 3 characters"
+          },
+          size:{
+                 required:"A size is needed",
+                 min: "The minimum average response is 50",
+                 max: "The maximum average response is 600"
+          } 
+    }
   })
+
 });
