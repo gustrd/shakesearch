22afb9ec1b3ddf925c77313cdbc6e5bb503383d5
gustrd <gustrd@gmail.com>
2023-03-22 10:09:30 -0300

    feat - form with selector for advanced configurations.

--- static/app.js
+++ static/app.js
@@ -118,3 +118,12 @@ $(document).ready(function() {
   })
   
 });
+
+function toggleDivAdvancedConfigurations() {
+  var div = document.getElementById("advanced-configurations");
+  if (div.style.display === "none") {
+    div.style.display = "block";
+  } else {
+    div.style.display = "none";
+  }
+}
